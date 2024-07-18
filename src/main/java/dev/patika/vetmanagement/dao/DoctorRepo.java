676e6ffdc081c73f78c19335a91e0201f5778392
irmaktekin <irmaktekin97@gmail.com>
2024-07-18 19:52:07 +0300

    Dao is created for doctor entity.

--- src/main/java/dev/patika/vetmanagement/dao/DoctorRepo.java
+++ src/main/java/dev/patika/vetmanagement/dao/DoctorRepo.java
@@ -4,6 +4,6 @@ import dev.patika.vetmanagement.entities.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface DoctorRepo extends JpaRepository<Doctor,Integer> {
 }
