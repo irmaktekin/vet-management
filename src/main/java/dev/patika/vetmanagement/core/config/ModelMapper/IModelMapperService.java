460a8fcf6e72fddf694e0fd256072ceb9491226f
irmaktekin <irmaktekin97@gmail.com>
2024-07-18 20:00:14 +0300

    Model Mapper configuration is done.

--- src/main/java/dev/patika/vetmanagement/core/config/ModelMapper/IModelMapperService.java
+++ src/main/java/dev/patika/vetmanagement/core/config/ModelMapper/IModelMapperService.java
@@ -2,6 +2,7 @@ package dev.patika.vetmanagement.core.config.ModelMapper;
 
 import org.modelmapper.ModelMapper;
 
+
 public interface IModelMapperService  {
     ModelMapper forRequest ();
     ModelMapper forResponse();
