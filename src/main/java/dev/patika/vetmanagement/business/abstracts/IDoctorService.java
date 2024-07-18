0a20bed8b37eef2c2737ebb9311a685fc3e2df2a
irmaktekin <irmaktekin97@gmail.com>
2024-07-18 20:05:04 +0300

    Business layer for doctor entity is implemented.

--- src/main/java/dev/patika/vetmanagement/business/abstracts/IDoctorService.java
+++ src/main/java/dev/patika/vetmanagement/business/abstracts/IDoctorService.java
@@ -3,7 +3,6 @@ package dev.patika.vetmanagement.business.abstracts;
 import dev.patika.vetmanagement.entities.Doctor;
 import org.springframework.data.domain.Page;
 
-
 public interface IDoctorService {
     Doctor save(Doctor category);
     Page<Doctor> cursor (int page, int pageSize);
