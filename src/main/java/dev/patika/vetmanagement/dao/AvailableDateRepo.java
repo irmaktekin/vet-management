e5d4fdd0d6162a18d144609dcaa9356eb991f909
irmaktekin <irmaktekin97@gmail.com>
2024-07-19 17:53:44 +0300

    Dao is created for available date.

--- src/main/java/dev/patika/vetmanagement/dao/AvailableDateRepo.java
+++ src/main/java/dev/patika/vetmanagement/dao/AvailableDateRepo.java
@@ -1,2 +1,9 @@
-package dev.patika.vetmanagement.dao;public interface AvailableDateRepo {
+package dev.patika.vetmanagement.dao;
+
+import dev.patika.vetmanagement.entities.AvailableDate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AvailableDateRepo extends JpaRepository<AvailableDate, Integer> {
 }
