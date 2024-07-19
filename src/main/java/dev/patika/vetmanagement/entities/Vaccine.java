6ce13a179d52739e80cef33c0f63fb64df730aae
irmaktekin <irmaktekin97@gmail.com>
2024-07-19 21:18:28 +0300

    Vaccine and animal relationship is implemented.

--- src/main/java/dev/patika/vetmanagement/entities/Vaccine.java
+++ src/main/java/dev/patika/vetmanagement/entities/Vaccine.java
@@ -1,21 +1,29 @@
 package dev.patika.vetmanagement.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name="vaccine")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Vaccine {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     @Column(name="vaccine_id")
     private Long id;
-
     private String name;
     private String code;
     private LocalDate protectionStartDate;
     private LocalDate protectionFinishDate;
+
+    @ManyToMany(mappedBy = "vaccines")
+    private Set<Animal> animals = new HashSet<>();
 }
