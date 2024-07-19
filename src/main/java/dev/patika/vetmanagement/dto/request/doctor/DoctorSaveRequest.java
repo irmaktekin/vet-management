7e71ced2ef9c2bcef01224f18e03d3742d79e35c
irmaktekin <irmaktekin97@gmail.com>
2024-07-19 17:58:37 +0300

    Relation between the available date is implemented.

--- src/main/java/dev/patika/vetmanagement/dto/request/doctor/DoctorSaveRequest.java
+++ src/main/java/dev/patika/vetmanagement/dto/request/doctor/DoctorSaveRequest.java
@@ -1,17 +1,21 @@
 package dev.patika.vetmanagement.dto.request.doctor;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class DoctorSaveRequest {
     private String name;
     private String phone;
     private String mail;
     private String address;
     private String city;
-    
+    @NotNull
+    private List<Long> availableDateIds;
 }
