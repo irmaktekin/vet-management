3d2bf7df7c58cc37ce4634d693a1a2e0a3082337
irmaktekin <irmaktekin97@gmail.com>
2024-07-19 22:01:09 +0300

    Annotations are added for getter and setter methods.

--- src/main/java/dev/patika/vetmanagement/dto/response/customer/CustomerSaveResponse.java
+++ src/main/java/dev/patika/vetmanagement/dto/response/customer/CustomerSaveResponse.java
@@ -1,5 +1,12 @@
 package dev.patika.vetmanagement.dto.response.customer;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CustomerSaveResponse {
     private Long id;
     private String name;
