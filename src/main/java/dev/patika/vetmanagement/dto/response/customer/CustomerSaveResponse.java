77fb9d7e6a5315350f2272cbc8cc48dca7ce85ca
irmaktekin <irmaktekin97@gmail.com>
2024-07-19 21:48:50 +0300

    Customer response is created.

---
+++ src/main/java/dev/patika/vetmanagement/dto/response/customer/CustomerSaveResponse.java
@@ -0,0 +1,9 @@
+package dev.patika.vetmanagement.dto.response.customer;
+
+public class CustomerSaveResponse {
+    private Long id;
+    private String name;
+    private String phone;
+    private String address;
+    private String city;
+}
