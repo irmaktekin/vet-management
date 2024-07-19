c5a7bf045494ed29190dcf6bb6c630611edb4c43
irmaktekin <irmaktekin97@gmail.com>
2024-07-19 23:31:37 +0300

    Response dto is created for animal entity.

---
+++ src/main/java/dev/patika/vetmanagement/dto/response/animal/AnimalResponse.java
@@ -0,0 +1,25 @@
+package dev.patika.vetmanagement.dto.response.animal;
+
+import dev.patika.vetmanagement.entities.Vaccine;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AnimalResponse {
+    private String name;
+    private String species;
+    private String breed;
+    private String gender;
+    private String colour;
+    private LocalDate dateOfBirth;
+    private Set<Vaccine> vaccines = new HashSet<>();
+    private Long customerId;
+    private Long doctorId;
+}
