fa3ede2ab42adb02370211849f8b6656d7031429
irmaktekin <irmaktekin97@gmail.com>
2024-07-19 23:11:00 +0300

    Response object for vaccine entity is created.

---
+++ src/main/java/dev/patika/vetmanagement/dto/response/vaccine/VaccineResponse.java
@@ -0,0 +1,19 @@
+package dev.patika.vetmanagement.dto.response.vaccine;
+
+import jakarta.annotation.security.DenyAll;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VaccineResponse {
+    private String name;
+    private String code;
+    private LocalDate protectionStartDate;
+    private LocalDate protectionFinishDate;
+
+}
