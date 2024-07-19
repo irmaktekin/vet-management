b65ab6eb3a33299f99e8e43d2c3f3b3397c36c1f
irmaktekin <irmaktekin97@gmail.com>
2024-07-19 21:44:35 +0300

    Customer service methods are implemented.

---
+++ src/main/java/dev/patika/vetmanagement/business/concretes/ICustomerManager.java
@@ -0,0 +1,40 @@
+package dev.patika.vetmanagement.business.concretes;
+
+import dev.patika.vetmanagement.business.abstracts.ICustomerService;
+import dev.patika.vetmanagement.dao.CustomerRepo;
+import dev.patika.vetmanagement.entities.Customer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ICustomerManager implements ICustomerService {
+    private final CustomerRepo customerRepo;
+
+    public ICustomerManager(CustomerRepo customerRepo) {
+        this.customerRepo = customerRepo;
+    }
+
+    @Override
+    public Customer save(Customer customer) {
+        return this.customerRepo.save(customer);
+    }
+
+    @Override
+    public Page<Customer> cursor(int page, int pageSize) {
+        Pageable pageable = PageRequest.of(page,pageSize);
+        return this.customerRepo.findAll(pageable);
+    }
+
+    @Override
+    public Customer get(long id) {
+        return this.customerRepo.findById((int)id).orElseThrow();
+    }
+
+    @Override
+    public boolean delete(int id) {
+        Customer customer = this.get(id);
+        return true;
+    }
+}
