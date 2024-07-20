5b03e1a48f4fc51578b7f63e5167f1c69ceb86de
irmaktekin <irmaktekin97@gmail.com>
2024-07-21 02:50:49 +0300

    update method is declared for customer service.

--- src/main/java/dev/patika/vetmanagement/business/abstracts/ICustomerService.java
+++ src/main/java/dev/patika/vetmanagement/business/abstracts/ICustomerService.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 public interface ICustomerService {
     Customer save(Customer customer);
+    Customer update(Customer customer);
     Page<Customer> cursor (int page, int pageSize);
     Customer get(long id);
     boolean delete(int id);
