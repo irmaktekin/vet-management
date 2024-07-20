1f00bb430956e9374b4ab8165d54915db30519da
irmaktekin <irmaktekin97@gmail.com>
2024-07-20 15:29:16 +0300

    Methods for filtering animals with name and customer name are declared

--- src/main/java/dev/patika/vetmanagement/business/abstracts/IAnimalService.java
+++ src/main/java/dev/patika/vetmanagement/business/abstracts/IAnimalService.java
@@ -4,10 +4,15 @@ import dev.patika.vetmanagement.entities.Animal;
 import dev.patika.vetmanagement.entities.Doctor;
 import org.springframework.data.domain.Page;
 
+import java.util.List;
+
 public interface IAnimalService {
     Animal save(Animal animal);
     Page<Animal> cursor (int page, int pageSize);
     Animal get(long id);
     boolean delete(int id);
+    List<Animal> findByName(String name);
+    List<Animal> findByCustomerName(String customerName);
+
 
 }
