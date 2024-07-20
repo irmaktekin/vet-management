148bf881d31fd5a8ccfea56d2e99eff34fb838fa
irmaktekin <irmaktekin97@gmail.com>
2024-07-20 15:36:11 +0300

    Method is added to service layer for filtering the customers by name.

--- src/main/java/dev/patika/vetmanagement/business/abstracts/ICustomerService.java
+++ src/main/java/dev/patika/vetmanagement/business/abstracts/ICustomerService.java
@@ -1,12 +1,17 @@
 package dev.patika.vetmanagement.business.abstracts;
 
+import dev.patika.vetmanagement.entities.Animal;
 import dev.patika.vetmanagement.entities.AvailableDate;
 import dev.patika.vetmanagement.entities.Customer;
 import org.springframework.data.domain.Page;
+
+import java.util.List;
 
 public interface ICustomerService {
     Customer save(Customer customer);
     Page<Customer> cursor (int page, int pageSize);
     Customer get(long id);
     boolean delete(int id);
+    List<Customer> findByName(String name);
+
 }
