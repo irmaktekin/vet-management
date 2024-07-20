aadc0651de31fc8062e5067c0673b88bb54cf272
irmaktekin <irmaktekin97@gmail.com>
2024-07-21 02:49:08 +0300

    Emndpoints for update and delete of customer is implemented.

--- src/main/java/dev/patika/vetmanagement/api/CustomerController.java
+++ src/main/java/dev/patika/vetmanagement/api/CustomerController.java
@@ -2,10 +2,14 @@ package dev.patika.vetmanagement.api;
 
 import dev.patika.vetmanagement.business.abstracts.ICustomerService;
 import dev.patika.vetmanagement.core.config.ModelMapper.IModelMapperService;
+import dev.patika.vetmanagement.core.result.Result;
 import dev.patika.vetmanagement.core.result.ResultData;
 import dev.patika.vetmanagement.core.utilities.ResultHelper;
+import dev.patika.vetmanagement.dto.request.animal.AnimalUpdateRequest;
 import dev.patika.vetmanagement.dto.request.availabledate.AvailableDateSaveRequest;
 import dev.patika.vetmanagement.dto.request.customer.CustomerSaveRequest;
+import dev.patika.vetmanagement.dto.request.customer.CustomerUpdateRequest;
+import dev.patika.vetmanagement.dto.response.animal.AnimalResponse;
 import dev.patika.vetmanagement.dto.response.availabledate.AvailableDateResponse;
 import dev.patika.vetmanagement.dto.response.customer.CustomerSaveResponse;
 import dev.patika.vetmanagement.entities.Animal;
@@ -43,6 +47,21 @@ public class CustomerController {
     @GetMapping("/filterByName")
     public List<Customer> filterByName(@RequestParam String name) {
         return iCustomerService.findByName(name);
+    }
+    //Delete the customer
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Result delete(@PathVariable("id") int id){
+        this.iCustomerService.delete(id);
+        return ResultHelper.ok();
+    }
+    //update customer
+    @PutMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public ResultData<CustomerSaveResponse> update(@Valid @RequestBody CustomerUpdateRequest customerUpdateRequest){
+        Customer customer = this.iModelMapperService.forRequest().map(customerUpdateRequest,Customer.class);
+        this.iCustomerService.update(customer);
+        return ResultHelper.success(this.iModelMapperService.forResponse().map(customer,CustomerSaveResponse.class));
     }
 
 }
