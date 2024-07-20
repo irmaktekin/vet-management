3349ab909ff038ea18a8d10d5ed8ec35bdeb264a
irmaktekin <irmaktekin97@gmail.com>
2024-07-21 02:58:34 +0300

    Dto for vaccine update is created..

---
+++ src/main/java/dev/patika/vetmanagement/dto/request/vaccine/VaccineUpdateRequest.java
@@ -0,0 +1,23 @@
+package dev.patika.vetmanagement.dto.request.vaccine;
+
+import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VaccineUpdateRequest {
+    private long id;
+    private String name;
+
+    @NotNull
+    private String code;
+    private LocalDate protectionStartDate;
+    private LocalDate protectionFinishDate;
+
+}
