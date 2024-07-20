fc2f9c2fecd4f74e980e3d7e8e94029f1604289d
irmaktekin <irmaktekin97@gmail.com>
2024-07-21 02:50:19 +0300

    update method is declared for appointment service.

--- src/main/java/dev/patika/vetmanagement/business/abstracts/IAppointmentService.java
+++ src/main/java/dev/patika/vetmanagement/business/abstracts/IAppointmentService.java
@@ -1,10 +1,12 @@
 package dev.patika.vetmanagement.business.abstracts;
 import dev.patika.vetmanagement.entities.Appointment;
 import dev.patika.vetmanagement.entities.AvailableDate;
+import dev.patika.vetmanagement.entities.Customer;
 import org.springframework.data.domain.Page;
 
 public interface IAppointmentService {
     Appointment save(Appointment appointment);
+    Appointment update(Appointment appointment);
     Page<Appointment> cursor (int page, int pageSize);
     Appointment get(long id);
     boolean delete(int id);
