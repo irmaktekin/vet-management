9e9529dfb0b35b0e8d2895aee561d6ea8518203e
irmaktekin <irmaktekin97@gmail.com>
2024-07-20 15:27:41 +0300

    Method for filtering animals with customer name is implemented.

--- src/main/java/dev/patika/vetmanagement/dao/AnimalRepo.java
+++ src/main/java/dev/patika/vetmanagement/dao/AnimalRepo.java
@@ -2,8 +2,16 @@ package dev.patika.vetmanagement.dao;
 
 import dev.patika.vetmanagement.entities.Animal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AnimalRepo extends JpaRepository<Animal,Integer> {
+    List<Animal> findByName(String name);
+
+    @Query("SELECT a FROM Animal a JOIN a.customer c WHERE c.name = :customerName")
+    List<Animal> findByCustomerName(String customerName);
+
 }
