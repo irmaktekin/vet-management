361290d86e46aa20ff97fb113cd23fc359dfa145
irmaktekin <irmaktekin97@gmail.com>
2024-07-20 03:14:26 +0300

    Repository annotation is added to the Vaccine dao.

--- src/main/java/dev/patika/vetmanagement/dao/VaccineRepo.java
+++ src/main/java/dev/patika/vetmanagement/dao/VaccineRepo.java
@@ -1,7 +1,10 @@
 package dev.patika.vetmanagement.dao;
 
+import dev.patika.vetmanagement.entities.Vaccine;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface VaccineRepo extends JpaRepository {
+@Repository
+public interface VaccineRepo extends JpaRepository<Vaccine,Integer> {
 
 }
