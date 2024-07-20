06ee7cf492415b71cd497afdd9833bf2f81db26e
irmaktekin <irmaktekin97@gmail.com>
2024-07-20 03:14:54 +0300

    Repository annotation is added to the Customer dao.

--- src/main/java/dev/patika/vetmanagement/dao/CustomerRepo.java
+++ src/main/java/dev/patika/vetmanagement/dao/CustomerRepo.java
@@ -1,2 +1,9 @@
-package dev.patika.vetmanagement.dao;public interface CustomerDao {
+package dev.patika.vetmanagement.dao;
+
+import dev.patika.vetmanagement.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepo extends JpaRepository<Customer,Integer> {
 }
