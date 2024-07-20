d0b88c15d4de9b021c8d8e33f50a9ee6ca9a952c
irmaktekin <irmaktekin97@gmail.com>
2024-07-21 02:52:52 +0300

    Method for finding vaccines by code and name is added to service.

--- src/main/java/dev/patika/vetmanagement/dao/VaccineRepo.java
+++ src/main/java/dev/patika/vetmanagement/dao/VaccineRepo.java
@@ -1,17 +1,23 @@
 package dev.patika.vetmanagement.dao;
 
 import dev.patika.vetmanagement.entities.Animal;
+import dev.patika.vetmanagement.entities.Customer;
 import dev.patika.vetmanagement.entities.Vaccine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface VaccineRepo extends JpaRepository<Vaccine,Integer> {
     @Query("SELECT v FROM Vaccine v JOIN v.animals a WHERE a.id = :animalId")
     List<Vaccine> findByAnimalId(@Param("animalId")int animalId);
+    @Query("SELECT v FROM Vaccine v WHERE v.code = :code AND v.name = :name")
+    Optional<Vaccine> findVaccinesByCodeAndName(@Param("code") String code, @Param("name") String name);
+
 
 }
