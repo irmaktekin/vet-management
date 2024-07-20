7b91d205028a4f316b10b54ee71613022bd99e0e
irmaktekin <irmaktekin97@gmail.com>
2024-07-20 15:32:34 +0300

    Method for filtering customers by name is implemented in customer dao.

--- src/main/java/dev/patika/vetmanagement/dao/CustomerRepo.java
+++ src/main/java/dev/patika/vetmanagement/dao/CustomerRepo.java
@@ -1,9 +1,14 @@
 package dev.patika.vetmanagement.dao;
 
+import dev.patika.vetmanagement.entities.Animal;
 import dev.patika.vetmanagement.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CustomerRepo extends JpaRepository<Customer,Integer> {
+    List<Customer> findByName(String name);
+
 }
