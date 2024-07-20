6cd4032e4ec54ec5185829d02b0ea8cf8c98b6ec
irmaktekin <irmaktekin97@gmail.com>
2024-07-20 03:18:39 +0300

    Available dates are added to the api for doctor entity's save request.

--- src/main/java/dev/patika/vetmanagement/api/DoctorController.java
+++ src/main/java/dev/patika/vetmanagement/api/DoctorController.java
@@ -1,4 +1,5 @@
 package dev.patika.vetmanagement.api;
+import dev.patika.vetmanagement.business.abstracts.IAvailableDateService;
 import dev.patika.vetmanagement.business.abstracts.IDoctorService;
 import dev.patika.vetmanagement.core.config.ModelMapper.IModelMapperService;
 import dev.patika.vetmanagement.core.result.Result;
@@ -7,29 +8,47 @@ import dev.patika.vetmanagement.core.utilities.ResultHelper;
 import dev.patika.vetmanagement.dto.request.doctor.DoctorSaveRequest;
 import dev.patika.vetmanagement.dto.response.CursorResponse;
 import dev.patika.vetmanagement.dto.response.doctor.DoctorResponse;
+import dev.patika.vetmanagement.entities.AvailableDate;
 import dev.patika.vetmanagement.entities.Doctor;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 @RestController
 @RequestMapping("/v1/doctors")
 public class DoctorController {
     private final IDoctorService iDoctorService;
     private final IModelMapperService iModelMapperService;
+    private final IAvailableDateService availableDateService;
 
-    public DoctorController(IDoctorService iDoctorService, IModelMapperService iModelMapperService) {
+    public DoctorController(IDoctorService iDoctorService, IModelMapperService iModelMapperService, IAvailableDateService availableDateService) {
         this.iDoctorService = iDoctorService;
         this.iModelMapperService = iModelMapperService;
+        this.availableDateService = availableDateService;
     }
 
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
     public ResultData<DoctorResponse> save(@Valid @RequestBody DoctorSaveRequest doctorSaveRequest){
         Doctor doctor = this.iModelMapperService.forRequest().map(doctorSaveRequest,Doctor.class);
+        Set<AvailableDate> availableDates = new HashSet<>();
+        System.out.println(doctorSaveRequest.getAvailableDateIds());
+        for (Long id : doctorSaveRequest.getAvailableDateIds()) {
+            AvailableDate availableDate = this.availableDateService.get(id);
+            availableDates.add(availableDate);
+        }
+        doctor.setAvailableDates(availableDates);
         this.iDoctorService.save(doctor);
-        return ResultHelper.created(this.iModelMapperService.forResponse().map(doctor,DoctorResponse.class));
+
+        return ResultHelper.created(this.iModelMapperService.forResponse().map(doctor, DoctorResponse.class));
+
+
     }
 
     //Get all doctors with pagination
