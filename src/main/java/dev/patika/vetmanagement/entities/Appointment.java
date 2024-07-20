7ec5b82182bd91c21472e037390164b94e45704b
irmaktekin <irmaktekin97@gmail.com>
2024-07-20 03:11:10 +0300

    Relation for appointment entity is implemented.

--- src/main/java/dev/patika/vetmanagement/entities/Appointment.java
+++ src/main/java/dev/patika/vetmanagement/entities/Appointment.java
@@ -1,14 +1,18 @@
 package dev.patika.vetmanagement.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.print.Doc;
 import java.time.LocalDate;
 
 @Entity
-@Table(name="appointmentdate")
-
+@Table(name="appointment")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Appointment {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
@@ -17,4 +21,12 @@ public class Appointment {
     @Column(name="appointment_date")
 
     private LocalDate appointmentDate;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "doctor_id", nullable = false)
+    private Doctor doctor;
 }
