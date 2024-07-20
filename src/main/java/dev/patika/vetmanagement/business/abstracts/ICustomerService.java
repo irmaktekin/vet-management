1bd1f41e2dfc2d05b1b0af00a480939ca15cfd89
irmaktekin <irmaktekin97@gmail.com>
2024-07-20 03:16:46 +0300

    Customer service implemented for customer entity.

--- src/main/java/dev/patika/vetmanagement/business/abstracts/ICustomerService.java
+++ src/main/java/dev/patika/vetmanagement/business/abstracts/ICustomerService.java
@@ -1,2 +1,12 @@
-package dev.patika.vetmanagement.business.abstracts;public interface ICustomerService {
+package dev.patika.vetmanagement.business.abstracts;
+
+import dev.patika.vetmanagement.entities.AvailableDate;
+import dev.patika.vetmanagement.entities.Customer;
+import org.springframework.data.domain.Page;
+
+public interface ICustomerService {
+    Customer save(Customer customer);
+    Page<Customer> cursor (int page, int pageSize);
+    Customer get(long id);
+    boolean delete(int id);
 }
