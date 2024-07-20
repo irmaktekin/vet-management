e034f237ccdf008ac51b1bd41dc29d2675ce7148
irmaktekin <irmaktekin97@gmail.com>
2024-07-20 03:08:26 +0300

    Jsonignore is used for hiding field in response for vaccine entity

--- src/main/java/dev/patika/vetmanagement/entities/Vaccine.java
+++ src/main/java/dev/patika/vetmanagement/entities/Vaccine.java
@@ -1,9 +1,8 @@
 package dev.patika.vetmanagement.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.util.HashSet;
@@ -14,6 +13,8 @@ import java.util.Set;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class Vaccine {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
@@ -23,6 +24,8 @@ public class Vaccine {
     private String code;
     private LocalDate protectionStartDate;
     private LocalDate protectionFinishDate;
+
+    @JsonIgnore // Ignore animals during serialization
 
     @ManyToMany(mappedBy = "vaccines")
     private Set<Animal> animals = new HashSet<>();
