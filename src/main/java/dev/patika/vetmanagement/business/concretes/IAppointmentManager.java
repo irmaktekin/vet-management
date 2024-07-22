fd069958a6778f99083b5992de452b79319050a2
irmaktekin <irmaktekin97@gmail.com>
2024-07-23 00:27:00 +0300

    Method for the check of appointments are added.

--- src/main/java/dev/patika/vetmanagement/business/concretes/IAppointmentManager.java
+++ src/main/java/dev/patika/vetmanagement/business/concretes/IAppointmentManager.java
@@ -1,13 +1,19 @@
 package dev.patika.vetmanagement.business.concretes;
 
 import dev.patika.vetmanagement.business.abstracts.IAppointmentService;
+import dev.patika.vetmanagement.core.exception.AppointmentConflictException;
+import dev.patika.vetmanagement.core.utilities.AppointmentTimeValidator;
 import dev.patika.vetmanagement.dao.AppointmentRepo;
 import dev.patika.vetmanagement.entities.Appointment;
-import dev.patika.vetmanagement.entities.Customer;
+import dev.patika.vetmanagement.entities.Doctor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class IAppointmentManager implements IAppointmentService {
@@ -20,6 +26,18 @@ public class IAppointmentManager implements IAppointmentService {
 
     @Override
     public Appointment save(Appointment appointment) {
+        if (!AppointmentTimeValidator.isHourAligned(appointment.getAppointmentDate())) {
+            throw new IllegalArgumentException("Appointment time must be at the start of the hour.");
+        }
+
+        // Check if an appointment exists for the given doctor at the given time
+        Optional<Appointment> existingAppointment = appointmentRepo
+                .findByDoctorIdAndAppointmentDate(appointment.getDoctor().getId(), appointment.getAppointmentDate());
+
+        if (existingAppointment.isPresent()) {
+            throw new AppointmentConflictException("There is already an appointment at this time.");
+        }
+
         return this.appointmentRepo.save(appointment);
     }
 
@@ -46,4 +64,16 @@ public class IAppointmentManager implements IAppointmentService {
         this.appointmentRepo.delete(appointment);
         return true;
     }
+
+    @Override
+    public List<Appointment> findAppointmentsByDoctorAndDateRange(String doctor, LocalDateTime startDate, LocalDateTime endDate) {
+        return appointmentRepo.findByDoctorNameAndAppointmentDateBetween(doctor, startDate, endDate);
+    }
+
+    @Override
+    public List<Appointment> findAppointmentsByAnimalAndDateRange(String animalName, LocalDateTime startDate, LocalDateTime endDate) {
+        return appointmentRepo.findByAnimalNameAppointmentDateBetween(animalName, startDate, endDate);
+    }
+
+
 }
