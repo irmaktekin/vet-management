ae055c85580c3ed89f33b078d0b14a2a6763a777
irmaktekin <irmaktekin97@gmail.com>
2024-07-23 00:25:18 +0300

    Appointment filter is implemented.

--- src/main/java/dev/patika/vetmanagement/business/abstracts/IAppointmentService.java
+++ src/main/java/dev/patika/vetmanagement/business/abstracts/IAppointmentService.java
@@ -2,7 +2,11 @@ package dev.patika.vetmanagement.business.abstracts;
 import dev.patika.vetmanagement.entities.Appointment;
 import dev.patika.vetmanagement.entities.AvailableDate;
 import dev.patika.vetmanagement.entities.Customer;
+import dev.patika.vetmanagement.entities.Doctor;
 import org.springframework.data.domain.Page;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface IAppointmentService {
     Appointment save(Appointment appointment);
@@ -10,5 +14,8 @@ public interface IAppointmentService {
     Page<Appointment> cursor (int page, int pageSize);
     Appointment get(long id);
     boolean delete(int id);
+    List<Appointment> findAppointmentsByDoctorAndDateRange(String doctorName, LocalDateTime startDate, LocalDateTime endDate);
+    List<Appointment> findAppointmentsByAnimalAndDateRange(String animalName, LocalDateTime startDate, LocalDateTime endDate);
+
 }
 
