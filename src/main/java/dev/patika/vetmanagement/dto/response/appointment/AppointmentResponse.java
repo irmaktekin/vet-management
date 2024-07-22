f73333eacf610b9dab8d34d05b67bfd558363482
irmaktekin <irmaktekin97@gmail.com>
2024-07-23 00:23:17 +0300

    Local date is converted to LocalDateTime.

--- src/main/java/dev/patika/vetmanagement/dto/response/appointment/AppointmentResponse.java
+++ src/main/java/dev/patika/vetmanagement/dto/response/appointment/AppointmentResponse.java
@@ -5,12 +5,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class AppointmentResponse {
-    private LocalDate appointmentDate;
+    private LocalDateTime appointmentDate;
     private Long customerId;
     private Long doctorId;
 }
